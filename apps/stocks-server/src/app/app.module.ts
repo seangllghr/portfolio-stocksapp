dd881cf2af15663b9d86f44fafd2d34941bc0d5a
Sean Gallagher <seangllghr@gmail.com>
2021-11-21 02:09:19 -0500

    scaffold controller

--- apps/stocks-server/src/app/app.module.ts
+++ apps/stocks-server/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StockController } from './stock/stock.controller';
 import { StockModule } from './stock/stock.module';
 
 @Module({
   imports: [StockModule],
-  controllers: [AppController],
+  controllers: [AppController, StockController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/stocks-server/src/app/stock/stock.controller.ts
+++ apps/stocks-server/src/app/stock/stock.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 
 @Controller('stock')
-export class StockController {}
+export class StockController {
+  @Post()
+  create(): string {
+    return 'This action adds a new stock';
+  }
+
+  @Get(':symbol')
+  findBySymbol(@Param('symbol') symbol: string): string {
+    return `This route will return data about ${symbol}`;
+  }
+
+  @Put()
+  update(): string {
+    return 'This route will update a stock';
+  }
+
+  @Delete()
+  delete(): string {
+    return 'This route will delete a stock';
+  }
+}
