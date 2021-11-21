75711cfd02e89dfaafdbe09df5928abf72e15f01
Sean Gallagher <seangllghr@gmail.com>
2021-11-21 12:59:21 -0500

    install and configure mongoose
    We're using a deployment-local config that's gitignored to avoid leaks
    and allow for flexible deployments.

--- apps/stocks-server/src/app/app.module.ts
+++ apps/stocks-server/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StockController } from './stock/stock.controller';
 import { StockModule } from './stock/stock.module';
 import { StockService } from './stock/stock.service';
+import * as config from '../assets/config.json'
+
+const connectString = 'mongodb+srv://'
+  + `${config.connectString.user}:${config.connectString.pass}`
+  + `@${config.connectString.host}/${config.connectString.db}`
+  + `?${config.connectString.opts.join('&')}`
 
 @Module({
-  imports: [StockModule],
+  imports: [StockModule, MongooseModule.forRoot(connectString)],
   controllers: [AppController, StockController],
   providers: [AppService, StockService],
 })
