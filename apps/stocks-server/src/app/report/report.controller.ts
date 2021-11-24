5dcc9d0f580ec29109245d35f44108ffb1f4306d
Sean Gallagher <seangllghr@gmail.com>
2021-11-24 14:48:51 -0500

    implement stocks report

--- apps/stocks-server/src/app/report/report.controller.ts
+++ apps/stocks-server/src/app/report/report.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ReportService } from './report.service';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+  constructor(private reportService: ReportService) {}
+
+  @Post('stocks')
+  async getStocksReport(@Body('symbols') symbols: string[]) {
+    return await this.reportService.generateStockReport(symbols)
+  }
+
+}
--- apps/stocks-server/src/app/report/report.service.ts
+++ apps/stocks-server/src/app/report/report.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { StockDocument } from '../stock/schemas/interval.schema';
+import { Stock } from '../stock/schemas/stock.schema';
+import { StockService } from '../stock/stock.service';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectModel(Stock.name) private stockModel: Model<StockDocument>
+  ) {}
+
+  async generateStockReport(symbols: string[]) {
+    return await this.stockModel.find({ symbol: {$in: symbols} })
+  }
+
+}
