203ee51b2e00c677ac24c6bf7d02ea746a939928
Sean Gallagher <seangllghr@gmail.com>
2021-11-27 15:09:40 -0500

    implement `runOverviewUpdate`

--- apps/stocks-server/src/app/market-sync/market-sync.service.ts
+++ apps/stocks-server/src/app/market-sync/market-sync.service.ts
@@ -1,43 +1,46 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Cron } from '@nestjs/schedule';
 import { Model } from 'mongoose';
-import { StockDocument } from '../stock/schemas/interval.schema';
+import { firstValueFrom } from 'rxjs';
+import * as config from '../../assets/config.json';
 import { Stock } from '../stock/schemas/stock.schema';
 
 @Injectable()
 export class MarketSyncService {
   constructor(
-    @InjectModel(Stock.name) private stockModel: Model<StockDocument>
+    @InjectModel(Stock.name) private stockModel: Model<Stock>,
+    private httpService: HttpService
   ) {}
 
-  private readonly symbols: string[] = ['AAPL', 'GOOG', 'IBM'];
+  private readonly symbols: string[] = ['TSLA', 'AAPL', 'GOOG', 'IBM', ];
   private currentSymbol: number = 0;
   private updateSteps: string[] = ['overview', 'time series']
   private currentStep: number = 0;
 
   /**
-   * Declare an update task with a tick rate of 5 per minute.
+   * Declare an update task with a tick rate of one per 12 seconds (5/min).
    *
    * NOTE: AlphaVantage, the API we're using for market data, has a limit of
    * 5 API calls per minute at its free tier. Thus, our update service is built
    * around this limitation. For a production system, this could be relaxed.
    */
-  @Cron('*/5 * * * * *')
-  runUpdateTask() {
+  @Cron('*/12 * * * * *')
+  runUpdateTask(): void {
     this.runNextUpdate();
   }
 
   /**
    * This code executes the appropriate update action at each update tick
    */
-  async runNextUpdate() {
+  async runNextUpdate(): Promise<void> {
     switch (this.updateSteps[this.currentStep]) {
       // A switch statement here allows us to expand the update cycle as the
       // data model evolves. We switch on the string value for readability.
       case 'overview':
         await this.runOverviewUpdate();
-        this.currentStep++
+        this.currentStep++;
         break;
       case 'time series':
         await this.runTimeSeriesUpdate();
@@ -49,19 +52,35 @@ export class MarketSyncService {
       default:
         const errorMessage = `'${this.updateSteps[this.currentStep]}'`
           + ' is not a valid update step.'
-          + ` Valid steps are: '${this.updateSteps.join(', ')}'`
-        throw new RangeError(errorMessage)
+          + ` Valid steps are: '${this.updateSteps.join(', ')}'`;
+        throw new RangeError(errorMessage);
     }
   }
 
-  async runOverviewUpdate() {
+  /**
+   * Update company overview information
+   *
+   * For now, we only update company overview data when the company doesn't yet
+   * exist in the database.
+   */
+  async runOverviewUpdate(): Promise<void> {
     const existingRecord = await this.stockModel.findOne({
       symbol: this.symbols[this.currentSymbol]
     })
-    console.log(`Overview update found company: ${existingRecord}`)
+    if (existingRecord) {
+      console.log(`Overview update found existing record for ${existingRecord.name}`);
+    } else {
+      const queryString = 'https://alphavantage.co/query?function=OVERVIEW'
+        + `&symbol=${this.symbols[this.currentSymbol]}`
+        + `&apikey=${config.upstreamAPI.key}`
+      const response = await firstValueFrom(this.httpService.get(queryString))
+      const overviewData = response.data
+      await this.stockModel.insertMany(overviewData)
+      console.log(`Added ${overviewData.Name} to the database`)
+    }
   }
 
-  async runTimeSeriesUpdate() {
+  async runTimeSeriesUpdate(): Promise<void> {
     console.log(`Time Series update: ${this.symbols[this.currentSymbol]}`)
   }
 
