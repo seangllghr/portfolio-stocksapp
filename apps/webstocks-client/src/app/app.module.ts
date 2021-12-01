d121829695ad66d6556c4452f6f9dc2a8f30e3e0
Sean Gallagher <seangllghr@gmail.com>
2021-12-01 09:55:55 -0500

    implement icon button

--- apps/webstocks-client/src/app/app.module.ts
+++ apps/webstocks-client/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faPlus, faHome } from '@fortawesome/free-solid-svg-icons';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, IconButtonComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faPlus, faHome);
+  }
+}
---
+++ apps/webstocks-client/src/app/components/icon-button/icon-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IconName } from '@fortawesome/fontawesome-svg-core';
+
+@Component({
+  selector: 'portfolio-stocksapp-icon-button',
+  templateUrl: './icon-button.component.html',
+  styleUrls: ['./icon-button.component.scss']
+})
+export class IconButtonComponent implements OnInit {
+  @Input() icon!: IconName;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
