4154902849bdfae132c0f5dee3c0ea0a59792407
Sean Gallagher <seangllghr@gmail.com>
2021-12-01 09:55:13 -0500

    implement application titlebar

--- apps/webstocks-client/src/app/app.module.ts
+++ apps/webstocks-client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
---
+++ apps/webstocks-client/src/app/components/layout/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-stocksapp-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  title = 'WebSTOCKS';
+
+}
