9caf311d9f1a8c90ceb90ccfba7d8164ea34490d
Sean Gallagher <seangllghr@gmail.com>
2021-12-03 15:56:32 -0500

    implement stocks service

--- apps/webstocks-client/src/app/services/stock.service.ts
+++ apps/webstocks-client/src/app/services/stock.service.ts
@@ -1,15 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { StockInterface as Stock } from '@portfolio-stocksapp/shared-data-model';
-import { Observable, of } from 'rxjs';
-import { STOCKS } from '../../stocks-list';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
+  private apiUrl = 'http://localhost:3333/api/stock/';
+
+  constructor(private http: HttpClient) {}
 
   getStocks(): Observable<Stock[]> {
-    const stocks: Observable<Stock[]> = of(STOCKS);
-    return stocks;
+    return this.http.get<Stock[]>(this.apiUrl);
   }
 }
