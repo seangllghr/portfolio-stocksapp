4645fdec9d44fb1848938ddcd35a8d905d27fa97
Sean Gallagher <seangllghr@gmail.com>
2021-12-04 16:39:32 -0500

    change import that I forgot to change

--- apps/webstocks-client/src/app/components/stock-detail/stock-detail.component.ts
+++ apps/webstocks-client/src/app/components/stock-detail/stock-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Stock } from '@portfolio-stocksapp/shared-data-model';
+import { StockInterface as Stock } from '@portfolio-stocksapp/shared-data-model';
 
 @Component({
   selector: 'webstocks-stock-detail',
