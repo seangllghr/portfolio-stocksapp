893694f29d907d6316cf80ec4d81383aa4e48c7a
Sean Gallagher <seangllghr@gmail.com>
2021-12-07 17:10:33 -0500

    reorganize imports/declarations

--- apps/webstocks-client/src/app/app.module.ts
+++ apps/webstocks-client/src/app/app.module.ts
@@ -6,10 +6,6 @@ import {
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { MenuButtonComponent } from './components/menu-button/menu-button.component';
 import {
   faPlus,
   faTimes,
@@ -18,11 +14,15 @@ import {
   faMinus,
   faRedo,
 } from '@fortawesome/free-solid-svg-icons';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuButtonComponent } from './components/menu-button/menu-button.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { StocksComponent } from './components/stocks/stocks.component';
 import { StocksItemComponent } from './components/stocks-item/stocks-item.component';
 import { StockDetailComponent } from './components/stock-detail/stock-detail.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes = [
   {
@@ -51,11 +51,11 @@ const routes = [
     AppComponent,
     MenuComponent,
     MenuButtonComponent,
+    NotFoundComponent,
     StocksComponent,
     StocksItemComponent,
     StockDetailComponent,
     WelcomeComponent,
-    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
